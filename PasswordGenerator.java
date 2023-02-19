64d991cdc2e701bdc102935275d4ad090f7bbeff
Patrick Moran <37051272+paktric@users.noreply.github.com>
2023-02-19 14:57:34 -0500

    Add files via upload

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -2,37 +2,38 @@ import java.security.SecureRandom;
 
 public class PasswordGenerator {
     // Define alphabet that password will be composed of
-    private static final String uCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final String lCaseLetters = uCaseLetters.toLowerCase();
-    private static final String numbers      = "0123456789";
-    private static final String specialChars = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/";
-    private static final String alphabet = uCaseLetters + lCaseLetters + numbers + specialChars;
+    private static final String U_CASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String L_CASE_LETTERS = U_CASE_LETTERS.toLowerCase();
+    private static final String NUMBERS        = "0123456789";
+    private static final String SPECIAL_CHARS  = "~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/";
+    private static final String ALPHABET = U_CASE_LETTERS + L_CASE_LETTERS + NUMBERS + SPECIAL_CHARS;
 
-    // Mutable variables
-    private static int passwordLength  = 16;
-    private static String password = "";
-    private static SecureRandom random = new SecureRandom();
+    private static final int DEFAULT_LENGTH  = 16;
+    private static final SecureRandom RANDOM = new SecureRandom();
 
     /**
      * Method to generate a random password with a default 16-digit length
      * @return Password as a String
      */
     static String newPassword(){
-        password = "";
-        for(int i=0; i<passwordLength; i++){
-            password += alphabet.charAt(random.nextInt(alphabet.length()));
+        String password = "";
+        for(int i=0; i<DEFAULT_LENGTH; i++){
+            password += ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length()));
         }
         return password;
     }
 
     /**
      * Method to generate a random password with a user-specified length
-     * @param n Length of password
+     * @param length Length of password
      * @return Password as a String
      */
-    static String newPassword(int n){
-        passwordLength = n;
-        return newPassword();
+    static String newPassword(int length){
+        String password = "";
+        for(int i=0; i<length; i++){
+            password += ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length()));
+        }
+        return password;
     }
-    
+
 }
