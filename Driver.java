e4f8bca5aff02dd982788c09e4ad7ea315a7bf9d
Patrick Moran <37051272+paktric@users.noreply.github.com>
2023-02-19 13:08:30 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -7,7 +7,7 @@ public class Driver {
         System.out.println("Random password 2:  " + PasswordGenerator.newPassword());
         System.out.println("Random password 3:  " + PasswordGenerator.newPassword());
 
-        System.out.println("\nPassword of length 8:  " + PasswordGenerator.newPassword(10));
+        System.out.println("\nPassword of length 8:   " + PasswordGenerator.newPassword(10));
         System.out.println("Password of length 12:  " + PasswordGenerator.newPassword(20));
         System.out.println("Password of length 32:  " + PasswordGenerator.newPassword(30));
 
